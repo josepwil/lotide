882cb26686aae970bfe8438f7ae46eb969d7a5ff
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-23 23:22:22 +0000

    function that retrieves the first element of an array

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed 🛑: ${actual} !== ${expected}`);
+  }
+};
+
+
+const head = function(array) {
+  return array[0];
+};
+
+
+
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["value"]), "value");
+assertEqual(head([]), undefined);
