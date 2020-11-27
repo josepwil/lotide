852371c01bc082b398583508ab53fa97d1abd102
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-27 23:31:47 +0000

    added recursion so that eqObjects can support sub-objects

--- eqObjects.js
+++ eqObjects.js
@@ -35,11 +35,17 @@ const eqObjects = function(object1, object2) {
   if (!checkLength(object1Keys, object2Keys)) {
     return false;
   }
-
   for (let key of object1Keys) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
-    } 
+    // if they are objects
+    if (typeof(object1[key]) === "object" && typeof(object2[key]) === "object") {
+      // if they are arrays
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        return eqArrays(object1[key], object2[key]);
+      } else {
+        // if they are not arrays and therefore true objects 
+        return eqObjects(object1[key], object2[key]);
+      }
+    }
     else if (object1[key] !== object2[key]) {
       return false;
     }
@@ -47,9 +53,16 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc)); // => true
+
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2)); // => false
+
