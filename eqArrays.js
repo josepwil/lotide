086eff82ec006b289974c6df5698cda611dcd341
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-28 00:33:40 +0000

    added resurcsion enabled eqArrays to support arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,29 +1,43 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed 🛑: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑 Assertion Failed 🛑: ${actual} !== ${expected}`);
+//   }
+// };
 
 // take in array, compare each value if not equal return false
 const eqArrays = function(arrayOne, arrayTwo) {
+  // if they're not the same length definitely not equal
   if (arrayOne.length !== arrayTwo.length) {
     return false;
   }
+  // loop over the first array
   for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
+    // check if item in array is an array itself - if yes call eqArrays on it
+    if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
+      if(!eqArrays(arrayOne[i], arrayTwo[i])) {
+        return false;
+      }
+    } else if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
   }
+  // if we haven't return false by now they must be the same
   return true;
 };
 
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+console.log(eqArrays([1, 2, [3]], [1, 2, 3])); // false
+console.log(eqArrays([[1, 2, [[3], 4]], 5, []], [[1, 2, [[3], 4]], 5, []])) // true
+console.log(eqArrays([[1, 2, [[3], 4]], 5, [7]], [[1, 2, [[3], 4]], 5, [6]])) // false
 
 
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
