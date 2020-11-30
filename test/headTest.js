046ef1801c27f1a3e2489609bd12e7926258524a
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-30 16:02:14 +0000

    added mocha and chai and implemented automated tests for head.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,30 @@
 const head = require('../head');
 const assertEqual = require('../assertEqual');
+const { assert } = require('chai');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["value"]), "value");
-assertEqual(head([]), undefined);
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns 'value' for ['value']", () => {
+    assert.strictEqual(head(['value']), 'value');
+  })
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  })
+});
