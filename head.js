088205d2cb90ffb6cf4d7e4c9ed528a76d968b0c
Joseph Williams <josephwilliamsjs@gmail.com>
2020-11-30 15:13:02 +0000

    refactored head.js and created headTest.js

--- head.js
+++ head.js
@@ -1,15 +1,5 @@
-const assertEqual = require('./assertEqual')
-
-
 const head = function(array) {
   return array[0];
 };
 
-
-
-
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["value"]), "value");
-assertEqual(head([]), undefined);
+module.exports = head;
